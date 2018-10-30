689ab33f99f916335d6f3fcfa56c98cd970ea298
Ralph Smith <ralphsmith80@gmail.com>
2018-10-30 11:50:03 -0600

    initial glu-3 work

---
+++ packages/react-scripts/config/glu.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const webpack = require('webpack');
+
+const alias = {
+  handlebars: 'handlebars/dist/handlebars.min.js',
+};
+
+const rules = [
+  {
+    test: /\.hbs$/,
+    loader: require.resolve('handlebars-loader'),
+    options: {
+      runtime: 'handlebars',
+      // prevent handlebars-loader from looking for files that match the name of any helper in hbs templates.
+      // We have several instances where the files are not helpers and this blackbox behavior seems unnecessary for now
+      rootRelative: '',
+
+      // Work around issue with unknown helpers at build time for any page that has partials
+      // partials will trigger a recompile of the page in the loader (as will finding new helpers and a few other things)
+      // On recompile handlebars-loader passes handlebars.precompile a knownHelpersOnly flag which makes it blow up if it
+      // encounters a helper that isn't already registered at build time.
+      // For now the work around is to list all helpers that are not registered at build time on pages with partials below.
+      knownHelpers: [
+        'date',
+        'amount',
+        'number',
+        'abbrNumber',
+        'money',
+        'documentRoot',
+        'loadingMessages',
+        'dateFormat',
+        'bool',
+        'stringContains',
+        'locale',
+        'currencySymbol',
+        'route',
+        'isEqual',
+        'collapsePanel',
+        'icon',
+      ],
+      exclude: 'node_modules(\\/|\\\\).*',
+
+      inlineRequires: 'images/',
+      debug: false,
+    },
+  },
+];
+
+const plugins = [
+  new webpack.ProvidePlugin({
+    // DO NOT provide '$', it causes a circular reference that results in an '{}'
+    // $: 'jquery',
+    jQuery: 'jquery',
+  }),
+];
+
+module.exports = {
+  alias,
+  rules,
+  plugins,
+};
--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Alert from './components/Alert';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Alert className="App-alert" />
         </header>
       </div>
     );
